4b057558c878493b9175f6a461d346467c337b8c
Aiwei <hezhili@snqu.com>
2023-04-03 10:09:47 +0800

    WidgetDemo

---
+++ WidgetDemo/WidgetA/WidgetABundle.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetABundle.swift
+//  WidgetA
+//
+//  Created by Aiwei on 2022/12/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct WidgetABundle: WidgetBundle {
+    var body: some Widget {
+        WidgetA()
+        WidgetALiveActivity()
+    }
+}
